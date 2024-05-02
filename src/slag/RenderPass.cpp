a8648cbebc1e840fb3ce5f91d8cca32f0dd84ffc
jshelton <joashel@gmail.com>
2024-05-02 13:25:19 -0600

    Started Render Graph

--- src/slag/RenderPass.cpp
+++ src/slag/RenderPass.cpp
@@ -1,5 +1,20 @@
 #include "RenderPass.h"
-
+#include <stdexcept>
 namespace slag
 {
+    RenderPass::RenderPass(std::string name, slag::RenderPass::PassType passType)
+    {
+        throw std::runtime_error("not implemented");
+    }
+
+    void RenderPass::addColorAttachment(std::string name, slag::AttachmentInfo &info)
+    {
+        throw std::runtime_error("not implemented");
+    }
+
+    RenderPass RenderPass::merge(RenderPass &with)
+    {
+        throw std::runtime_error("not implemented");
+        //return RenderPass(std::basic_string(), RenderPass::GRAPHICS);
+    }
 } // slag
