042ee5524f2171d0f64c0af171597abca6d79248
Josh Shelton <joashel@gmail.com>
2025-08-18 16:01:02 -0600

    working slag cube example

--- examples/cube/Main.cpp
+++ examples/cube/Main.cpp
@@ -5,20 +5,37 @@
 #include <glm/glm.hpp>
 
 #include "SDL_syswm.h"
+#include "glm/ext/matrix_clip_space.hpp"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include "glm/ext/matrix_transform.hpp"
+
 class DefaultResources: public slag::FrameResources
 {
 public:
     slag::CommandBuffer* commandBuffer=nullptr;
     slag::Semaphore* commandsFinished=nullptr;
+    slag::DescriptorPool* descriptorPool=nullptr;
+    slag::Buffer* globalsBuffer = nullptr;
+    slag::Buffer* instanceBuffer = nullptr;
+
     DefaultResources():slag::FrameResources()
     {
         commandBuffer = slag::CommandBuffer::newCommandBuffer(slag::GPUQueue::QueueType::GRAPHICS);
         commandsFinished = slag::Semaphore::newSemaphore();
+        descriptorPool = slag::DescriptorPool::newDescriptorPool();
+        globalsBuffer = slag::Buffer::newBuffer(sizeof(glm::mat4)*3,slag::Buffer::Accessibility::CPU_AND_GPU,slag::Buffer::UsageFlags::UNIFORM_BUFFER);
+        instanceBuffer = slag::Buffer::newBuffer(sizeof(glm::mat4),slag::Buffer::Accessibility::CPU_AND_GPU,slag::Buffer::UsageFlags::UNIFORM_BUFFER);
     }
     ~DefaultResources()override
     {
         delete commandBuffer;
         delete commandsFinished;
+        delete descriptorPool;
+        delete globalsBuffer;
+        delete instanceBuffer;
     }
 };
 
@@ -43,85 +60,82 @@ int main()
 {
     slag::initialize(slag::SlagInitInfo{.graphicsBackend = slag::GraphicsBackend::VULKAN_GRAPHICS_BACKEND, .slagDebugHandler = backEndMessage});
 
-    std::vector<glm::vec3> cverts =
-        {
-            {-0.5f, -0.5f, -0.5f},  // A 0
-            {0.5f, -0.5f, -0.5f},  // B 1
-            {0.5f,  0.5f, -0.5f},  // C 2
-            {-0.5f,  0.5f, -0.5f},  // D 3
-            {-0.5f, -0.5f,  0.5f},  // E 4
-            {0.5f, -0.5f,  0.5f},  // F 5
-            {0.5f,  0.5f,  0.5f},  // G 6
-            {-0.5f,  0.5f,  0.5f}, // H 7
+    std::vector<float> cverts =
+    {
+        -0.5f, 0.5f, -0.5f,  // A 0
+        0.5f, 0.5f, -0.5f,   // B 1
+        0.5f,  -0.5f, -0.5f,   // C 2
+        -0.5f,  -0.5f, -0.5f,  // D 3
+        -0.5f, 0.5f,  0.5f,  // E 4
+        0.5f, 0.5f,  0.5f,   // F 5
+        0.5f,  -0.5f,  0.5f,   // G 6
+        -0.5f,  -0.5f,  0.5f,   // H 7
 
-            {-0.5f,  0.5f, -0.5f}, // D 8
-            {-0.5f, -0.5f, -0.5f}, // A 9
-            {-0.5f, -0.5f,  0.5f}, // E 10
-            {-0.5f,  0.5f,  0.5f}, // H 11
-            {0.5f, -0.5f, -0.5f},  // B 12
-            {0.5f,  0.5f, -0.5f},  // C 13
-            {0.5f,  0.5f,  0.5f},  // G 14
-            {0.5f, -0.5f,  0.5f},  // F 15
+        -0.5f,  -0.5f, -0.5f,  // D 8
+        -0.5f, 0.5f, -0.5f,  // A 9
+        -0.5f, 0.5f,  0.5f,  // E 10
+        -0.5f,  -0.5f,  0.5f,   // H 11
+        0.5f, 0.5f, -0.5f,   // B 12
+        0.5f,  -0.5f, -0.5f,   // C 13
+        0.5f,  -0.5f,  0.5f,   // G 14
+        0.5f, 0.5f,  0.5f,   // F 15
 
-            {-0.5f, -0.5f, -0.5f}, // A 16
-            {0.5f, -0.5f, -0.5f},  // B 17
-            {0.5f, -0.5f,  0.5f},  // F 18
-            {-0.5f, -0.5f,  0.5f}, // E 19
-            {0.5f,  0.5f, -0.5f},  // C 20
-            {-0.5f,  0.5f, -0.5f}, // D 21
-            {-0.5f,  0.5f,  0.5f}, // H 22
-            {0.5f,  0.5f,  0.5f},  // G 23
-        };
-        std::vector<glm::vec2> cuvs =
-        {
-            {0.0f, 0.0f},  // A 0
-            {1.0f, 0.0f},  // B 1
-            {1.0f, 1.0f},  // C 2
-            {0.0f, 1.0f},  // D 3
-            {0.0f, 0.0f},  // E 4
-            {1.0f, 0.0f},   // F 5
-            {1.0f, 1.0f},   // G 6
-            {0.0f, 1.0f},   // H 7
+        -0.5f, 0.5f, -0.5f,   // A 16
+        0.5f, 0.5f, -0.5f,    // B 17
+        0.5f, 0.5f,  0.5f,    // F 18
+        -0.5f, 0.5f,  0.5f,   // E 19
+        0.5f,  -0.5f, -0.5f,   // C 20
+        -0.5f,  -0.5f, -0.5f, // D 21
+        -0.5f,  -0.5f,  0.5f,  // H 22
+        0.5f,  -0.5f,  0.5f,   // G 23
+    };
+    std::vector<float> cuvs =
+    {
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+    };
+    std::vector<uint16_t> cindexes =
+    {
+        0, 3, 2,
+         2, 1, 0,
+         4, 5, 6,
+         6, 7 ,4,
+         // left and right
+         11, 8, 9,
+         9, 10, 11,
+         12, 13, 14,
+         14, 15, 12,
+         // bottom and top
+         16, 17, 18,
+         18, 19, 16,
+         20, 21, 22,
+         22, 23, 20
+    };
 
-            {0.0f, 0.0f},  // D 8
-            {1.0f, 0.0f},  // A 9
-            {1.0f, 1.0f},  // E 10
-            {0.0f, 1.0f},  // H 11
-            {0.0f, 0.0f},   // B 12
-            {1.0f, 0.0f},   // C 13
-            {1.0f, 1.0f},   // G 14
-            {0.0f, 1.0f},   // F 15
-
-            {0.0f, 0.0f},  // A 16
-            {1.0f, 0.0f},   // B 17
-            {1.0f, 1.0f},   // F 18
-            {0.0f, 1.0f},  // E 19
-            {0.0f, 0.0f},  // C 20
-            {1.0f, 0.0f},  // D 21
-            {1.0f, 1.0f},  // H 22
-            {0.0f, 1.0f}  // G 23
-        };
-        std::vector<uint16_t> cindexes =
-        {
-            // front and back
-            0, 3, 2,
-            2, 1, 0,
-            4, 5, 6,
-            6, 7 ,4,
-            // left and right
-            11, 8, 9,
-            9, 10, 11,
-            12, 13, 14,
-            14, 15, 12,
-            // bottom and top
-            16, 17, 18,
-            18, 19, 16,
-            20, 21, 22,
-            22, 23, 20
-        };
-
-    slag::Buffer* cubeVerts = slag::Buffer::newBuffer(cverts.data(),cverts.size()*sizeof(glm::vec3),slag::Buffer::Accessibility::GPU,slag::Buffer::UsageFlags::VERTEX_BUFFER);
-    slag::Buffer* cubeUVs = slag::Buffer::newBuffer(cuvs.data(),cuvs.size()*sizeof(glm::vec2),slag::Buffer::Accessibility::GPU,slag::Buffer::UsageFlags::VERTEX_BUFFER);
+    slag::Buffer* cubeVerts = slag::Buffer::newBuffer(cverts.data(),cverts.size()*sizeof(float),slag::Buffer::Accessibility::GPU,slag::Buffer::UsageFlags::VERTEX_BUFFER);
+    slag::Buffer* cubeUVs = slag::Buffer::newBuffer(cuvs.data(),cuvs.size()*sizeof(float),slag::Buffer::Accessibility::GPU,slag::Buffer::UsageFlags::VERTEX_BUFFER);
     slag::Buffer* cubeIndices = slag::Buffer::newBuffer(cindexes.data(),cindexes.size()*sizeof(uint16_t),slag::Buffer::Accessibility::GPU,slag::Buffer::UsageFlags::INDEX_BUFFER);
 
 
@@ -134,10 +148,14 @@ int main()
     slag::ShaderCode* code[]{&vertexShader,&fragmentShader};
     slag::ShaderProperties properties{};
     slag::FrameBufferDescription frameBufferDescription{};
-    frameBufferDescription.colorTargets[0] = slag::Pixels::Format::B8G8R8A8_UNORM_SRGB;
+    frameBufferDescription.colorTargets[0] = slag::Pixels::Format::B8G8R8A8_UNORM;
     frameBufferDescription.depthTarget = slag::Pixels::Format::D32_FLOAT;
 
     slag::ShaderPipeline* texturedDepthPipeline = slag::ShaderPipeline::newShaderPipeline(code,2,properties,vertexPosUVDescription,frameBufferDescription);
+    int width, height, channels;
+    auto pixels = stbi_load("resources/textures/gradient.jpg", &width, &height,&channels,4);
+    auto texture = slag::Texture::newTexture(slag::Pixels::Format::R8G8B8A8_UNORM,slag::Texture::Type::TEXTURE_2D,slag::Texture::UsageFlags::SAMPLED_IMAGE,width,height,1,1, slag::Texture::SampleCount::ONE,pixels,1,1);
+    stbi_image_free(pixels);
     slag::Sampler* defaultSampler = slag::Sampler::newSampler(slag::SamplerParameters{});
 
 
@@ -160,11 +178,25 @@ int main()
     pd.details.x11.display = wmInfo.info.x11.display;
 #endif
 
-    auto swapChain = slag::SwapChain::newSwapChain(pd,WINDOW_WIDTH,WINDOW_HEIGHT,slag::SwapChain::PresentMode::QUEUE,2,slag::Pixels::Format::B8G8R8A8_UNORM_SRGB,slag::SwapChain::AlphaCompositing::IGNORE_ALPHA,createResource,swapChainRebuilt);
+    auto swapChain = slag::SwapChain::newSwapChain(pd,WINDOW_WIDTH,WINDOW_HEIGHT,slag::SwapChain::PresentMode::QUEUE,2,slag::Pixels::Format::B8G8R8A8_UNORM,slag::SwapChain::AlphaCompositing::IGNORE_ALPHA,createResource,swapChainRebuilt);
     auto queue = slag::slagGraphicsCard()->graphicsQueue();
+
+    slag::Buffer* vertexBuffers[]={cubeVerts,cubeUVs};
+    uint64_t vertexOffsets[]={0,0};
+    uint64_t vertexStrides[]{sizeof(glm::vec3),sizeof(glm::vec2)};
+
+    glm::mat4 instanceMatrix(1);
+
     bool keepOpen = true;
+    uint32_t last_tick_time = 0;
+    float delta = 0;
     while(keepOpen)
     {
+
+        uint32_t tick_time = SDL_GetTicks();
+        delta = ((float)tick_time - (float)last_tick_time)/1000;
+        last_tick_time = tick_time;
+
         SDL_Event event;
         while(SDL_PollEvent(&event))
         {
@@ -191,11 +223,54 @@ int main()
         {
             auto resources = frame->frameResources<DefaultResources>();
             auto commandBuffer = resources->commandBuffer;
-
+            auto descriptorPool = resources->descriptorPool;
+            auto globalsBuffer = resources->globalsBuffer;
+            auto instanceBuffer = resources->instanceBuffer;
             auto backBuffer = frame->backBuffer();
 
+            descriptorPool->reset();
+
             commandBuffer->begin();
-            commandBuffer->clearTexture(backBuffer,slag::ClearColor{.floats = {0,.2,.8,1}});
+            commandBuffer->bindDescriptorPool(descriptorPool);
+            commandBuffer->setViewPort(0,0,backBuffer->width(),backBuffer->height(),0,1);
+            commandBuffer->setScissors(slag::Rectangle{{0,0},{backBuffer->width(),backBuffer->height()}});
+
+            slag::Attachment colorAttachment{.texture = backBuffer,.autoClear = true, .clearValue = slag::ClearColor{.floats = {0,.2,1,1} }};
+            slag::Attachment depthAttachment{.texture = depthTexture,.autoClear = true };
+            depthAttachment.clearValue.depthStencil = {1,0};
+
+            commandBuffer->beginRendering(&colorAttachment,1,&depthAttachment,slag::Rectangle{{0,0},{backBuffer->width(),backBuffer->height()}});
+            commandBuffer->bindGraphicsShaderPipeline(texturedDepthPipeline);
+
+            glm::mat4 projection = glm::perspective(95.0f,(float)backBuffer->width()/(float)backBuffer->height(),.01f,100.0f);
+            glm::mat4 view(1.0f);
+            view = glm::translate(view,glm::vec3(0.0f,2.0f,5.0f));
+            view = glm::rotate(view,glm::radians(-20.0f),glm::vec3(1.0f,0.0f,0.0f));
+            view = glm::inverse(view);
+            glm::mat4 projectionView = projection * view;
+            auto globalHandle = globalsBuffer->as<glm::mat4>();
+            globalHandle[0] = projection;
+            globalHandle[1] = view;
+            globalHandle[2] = projectionView;
+
+            auto instanceHandle = instanceBuffer->as<glm::mat4>();
+            instanceHandle[0] = instanceMatrix;
+            instanceMatrix = glm::rotate(instanceMatrix,glm::radians(45.0f * delta),glm::vec3(0.0f,1.0f,0.0f));
+
+            auto globals = descriptorPool->makeBundle(texturedDepthPipeline->descriptorGroup(0));
+            globals.setUniformBuffer(0,0,globalsBuffer,0,globalsBuffer->size());
+            auto instance = descriptorPool->makeBundle(texturedDepthPipeline->descriptorGroup(1));
+            instance.setUniformBuffer(0,0,instanceBuffer,0,instanceBuffer->size());
+            instance.setTextureAndSampler(1,0,texture,defaultSampler);
+
+            commandBuffer->bindGraphicsDescriptorBundle(0,globals);
+            commandBuffer->bindGraphicsDescriptorBundle(1,instance);
+            commandBuffer->bindIndexBuffer(cubeIndices,slag::Buffer::IndexSize::UINT16,0);
+
+
+            commandBuffer->bindVertexBuffers(0,vertexBuffers,vertexOffsets,vertexStrides,2);
+            commandBuffer->drawIndexed(cindexes.size(),1,0,0,0);
+            commandBuffer->endRendering();
             commandBuffer->end();
 
             slag::QueueSubmissionBatch submissionBatch;
@@ -205,6 +280,7 @@ int main()
         }
     }
     delete swapChain;
+    delete texture;
     delete defaultSampler;
     delete depthTexture;
     delete texturedDepthPipeline;
--- tests/Utilities.cpp
+++ tests/Utilities.cpp
@@ -1,6 +1,5 @@
 #include "Utilities.h"
 #define STB_IMAGE_IMPLEMENTATION
-#include <iostream>
 #include <stb_image.h>
 #include <functional>
 #include <slag/backends/Backend.h>
@@ -14,7 +13,6 @@ namespace slag
     std::unique_ptr<Texture> utilities::loadTextureFromFile(const std::filesystem::path& path)
     {
         int width, height, channels;
-        stbi_image_free(nullptr);
         unique_ptr_custom<stbi_uc> data(stbi_load(path.string().c_str(), &width, &height,&channels,4),[](stbi_uc* uc){stbi_image_free(uc);});
         return std::unique_ptr<Texture>(Texture::newTexture(Pixels::Format::R8G8B8A8_UNORM,Texture::Type::TEXTURE_2D,Texture::UsageFlags::SAMPLED_IMAGE,width,height,1,1,Texture::SampleCount::ONE,data.get(),1,1));
     }
