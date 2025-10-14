d89b2f0a0038c1afad68158f9446e716296f7e83
Josh Shelton <joashel@gmail.com>
2025-10-14 15:27:07 -0600

    removed combined texture samplers from DescriptorPool page info

--- src/slag/core/DescriptorPool.h
+++ src/slag/core/DescriptorPool.h
@@ -11,28 +11,27 @@ namespace slag
     ///Describes how a descriptor pool should grow, should the underlying API require it
     struct DescriptorPoolPageInfo
     {
-        ///Number of samplers that can be created from the pool, may be ignored by underlying API
+        ///Number of sampler descriptors that can be created from the pool, may be ignored by underlying API
         uint32_t samplers=1000;
-        ///Number of sampledTextures that can be created from the pool, may be ignored by underlying API
+        ///Number of sampledTexture descriptors that can be created from the pool, may be ignored by underlying API
         uint32_t sampledTextures=1000;
-        ///Number of combinedSamplerTextures that can be created from the pool, may be ignored by underlying API
-        uint32_t combinedSamplerTextures=1000;
-        ///Number of storage textures that can be created from the pool, may be ignored by underlying API
+        ///Number of storage texture descriptors that can be created from the pool, may be ignored by underlying API
         uint32_t storageTextures=1000;
-        ///Number of uniform texel buffers that can be created from the pool, may be ignored by underlying API
+        ///Number of uniform texel buffer descriptors that can be created from the pool, may be ignored by underlying API
         uint32_t uniformTexelBuffers=1000;
-        ///Number of storage texel buffers that can be created from the pool, may be ignored by underlying API
+        ///Number of storage texel buffer descriptors that can be created from the pool, may be ignored by underlying API
         uint32_t storageTexelBuffers=1000;
-        ///Number of uniform buffers that can be created from the pool, may be ignored by underlying API
+        ///Number of uniform buffer descriptors that can be created from the pool, may be ignored by underlying API
         uint32_t uniformBuffers=1000;
-        ///Number of storage buffers that can be created from the pool, may be ignored by underlying API
+        ///Number of storage buffer descriptors that can be created from the pool, may be ignored by underlying API
         uint32_t storageBuffers=1000;
-        ///Number of input attachments that can be created from the pool, may be ignored by underlying API
+        ///Number of input attachment descriptors that can be created from the pool, may be ignored by underlying API
         uint32_t inputAttachments=1000;
-        ///Number of descriptor bundles that can be created from the pool, may be ignored by underlying API
+        ///Number of descriptor bundle descriptors that can be created from the pool, may be ignored by underlying API
         uint32_t accelerationStructures=0;
-        ///Number of descriptor bundles that can be created from the pool, may be ignored by underlying API
+        ///Number of descriptor bundle descriptors that can be created from the pool, may be ignored by underlying API
         uint32_t descriptorBundles=1000;
+
     };
     ///Pool for which descriptors can be assigned via DescriptorBundles
     class DescriptorPool
