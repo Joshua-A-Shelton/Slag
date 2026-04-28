0c7b5744995a846adf871cf1cb8031ad9000f383
Josh Shelton <joashel@gmail.com>
2026-04-28 13:09:43 -0600

    Vulkan Shaders working!

--- tests/tests/ShaderPipeline.cpp
+++ tests/tests/ShaderPipeline.cpp
@@ -32,47 +32,9 @@ TEST(ShaderPipeline, Sandbox)
     auto tindicesPtr = triangleIndices->as<uint16_t>();
     memcpy(tindicesPtr,tindexes.data(),tindexes.size()*sizeof(uint16_t));
 
-
-    auto vertex = slagTestsCreateShaderModule(card, "resources/tests/shaders/compiled/TexturedDepth.vertex");
-    auto fragment = slagTestsCreateShaderModule(card, "resources/tests/shaders/compiled/TexturedDepth.fragment");
-    std::vector<VertexBinding> vertexBindings =
-    {
-        VertexBinding(0,sizeof(float)*3,std::vector<VertexAttribute>{VertexAttribute("POSITION",PixelFormat::R32G32B32_FLOAT,0)}),
-        VertexBinding(1,sizeof(float)*2,std::vector<VertexAttribute>{VertexAttribute("UV_COORDINATES",PixelFormat::R32G32_FLOAT,0)})
-    };
-    VertexDescription vertexDesc(vertexBindings);
-    FramebufferDescription framebufferDesc{};
-    framebufferDesc.colorFormats[0] = PixelFormat::R8G8B8A8_UNORM;
-    framebufferDesc.depthFormat = PixelFormat::D32_FLOAT;
-    PipelineInputMappingBuilder pipelineBindingBuilder(card);
-    BufferLayout globalLayout(std::vector<StructMember>
-    {
-        StructMember("projection",GraphicsType::MATRIX_4X4,1,0,0),
-        StructMember("view",GraphicsType::MATRIX_4X4,1,64,64),
-        StructMember("projectionView",GraphicsType::MATRIX_4X4,1,128,128),
-    });
-    BufferLayout InstanceLayout(std::vector<StructMember>{StructMember("transform",GraphicsType::DOUBLE_MATRIX_4X4,1,0,0)});
-    DescriptorRange globalsDescriptor{.type = DescriptorRangeType::UNIFORM_BUFFER,.bindGroupIndex = 0,.firstBinding = 0,.bindingCount = 1,.offsetInDescriptorsFromTableStart = 0};
-    pipelineBindingBuilder.addDescriptorTable(&globalsDescriptor,1);
-    DescriptorRange samplerDescriptor{.type = DescriptorRangeType::SAMPLER,.bindGroupIndex = 1,.firstBinding = 0,.bindingCount = 1, .offsetInDescriptorsFromTableStart = 0};
-    pipelineBindingBuilder.addDescriptorTable(&samplerDescriptor,1);
-    DescriptorRange instance[] =
-    {
-        {.type = DescriptorRangeType::UNIFORM_BUFFER,.bindGroupIndex = 2,.firstBinding = 0,.bindingCount = 1,.offsetInDescriptorsFromTableStart = 0},
-        {.type = DescriptorRangeType::READONLY_RESOURCE,.bindGroupIndex = 2,.firstBinding = 1,.bindingCount = 1,.offsetInDescriptorsFromTableStart = 1}
-    };
-    pipelineBindingBuilder.addDescriptorTable(instance,2);
-    auto bindings = pipelineBindingBuilder.build();
-    auto pipeline = std::unique_ptr<ShaderPipeline>(card->newShaderPipeline(vertexDesc,vertex.get(), fragment.get(),&bindings,PipelineState{},framebufferDesc));
-    auto commandBuffer = std::unique_ptr<CommandBuffer>(card->newCommandBuffer(QueueType::GRAPHICS));
     auto colorTarget = std::unique_ptr<Texture>(card->newTexture2D(250,250,PixelFormat::R8G8B8A8_UNORM,TextureUsageFlags::COLOR_TARGET));
     auto depthTarget = std::unique_ptr<Texture>(card->newTexture2D(250,250,PixelFormat::D32_FLOAT,TextureUsageFlags::DEPTH_STENCIL_TARGET));
-    auto texture = utilities::loadTexture("resources\\tests\\textures\\gradient.jpg",card);
-    auto resultBuffer = std::unique_ptr<Buffer>(card->newBuffer(colorTarget->bufferSize(PixelAspect::COLOR),BufferCPUAccess::READ_WRITE));
-    auto finished = std::unique_ptr<Semaphore>(card->newSemaphore());
-    auto resourceHeap = std::unique_ptr<DescriptorHeap>(card->newDescriptorHeap(DescriptorHeapType::RESOURCE,256000));
-    auto samplerHeap = std::unique_ptr<DescriptorHeap>(card->newDescriptorHeap(DescriptorHeapType::SAMPLER,25600));
-    auto sampler = std::unique_ptr<Sampler>(card->newSampler());
+
     auto globals = std::unique_ptr<Buffer>(card->newBuffer(/*192*/256,BufferCPUAccess::WRITE_ONLY,BufferMemoryType::UNIFORM));
     auto transform = std::unique_ptr<Buffer>(card->newBuffer(/*64*/256,BufferCPUAccess::WRITE_ONLY,BufferMemoryType::UNIFORM));
     auto proj = glm::perspective(95.0f,(float)colorTarget->width()/(float)colorTarget->height(),.01f,100.0f);
@@ -89,79 +51,129 @@ TEST(ShaderPipeline, Sandbox)
     auto transformPtr = transform->as<glm::mat4>();
     transformPtr[0] = glm::rotate(glm::mat4(1),glm::radians(45.0f),glm::vec3(0.0f,1.0f,0.0f));
 
-    commandBuffer->begin();
-    commandBuffer->bindDescriptorHeaps(resourceHeap.get(),samplerHeap.get());
-    commandBuffer->setViewPort(0,0,250,250,0,1);
-    commandBuffer->setScissors(Rectangle{0,0,250,250});
-    Attachment colorAttachment{.texture = colorTarget.get(),.autoClear = true, .clearValue = ClearValue{.color{1.0f,.5f,.5f,1.0f}}};
-    Attachment depthAttachment{.texture = depthTarget.get(),.autoClear = true, .clearValue = ClearValue{.depthStencil = {.depth = 1, .stencil = 0}}};
-    commandBuffer->bindGraphicsPipeline(pipeline.get());
-    commandBuffer->beginRendering(&colorAttachment,1,&depthAttachment,Rectangle{0,0,250,250});
-    uint32_t offset = 0;
-    resourceHeap->writeUniformBufferDescriptor(bindings.input(0).location(),globals.get(),0,globals->size());
-    commandBuffer->setInputBindingTable(0,offset);
-    offset += bindings.input(2).location();
-    samplerHeap->writeSamplerDescriptor(0,sampler.get());
-    commandBuffer->setInputBindingTable(bindings.input(1).location(),0);
-    resourceHeap->writeUniformBufferDescriptor(offset,transform.get(),0,transform->size());
-    resourceHeap->writeSampledTextureDescriptor(offset+card->descriptorTableDetails().sampledTextureSize,texture.get());
-    commandBuffer->setInputBindingTable(bindings.input(2).location(),offset);
-    //offset+=bindings.descriptorTable(2).size();
 
-    commandBuffer->bindIndexBuffer(triangleIndices.get(),IndexBufferType::UINT_16,0);
-    Buffer* vertexBuffers[] {triangleVerts.get(),triangleUVs.get()};
-    uint64_t offsets[]{0,0};
-    uint64_t strides[]
-    {
-        sizeof(glm::vec3),
-        sizeof(glm::vec2)
+    auto vertex = slagTestsCreateShaderModule(card, "resources/tests/shaders/compiled/TexturedDepth.vertex");
+    auto fragment = slagTestsCreateShaderModule(card, "resources/tests/shaders/compiled/TexturedDepth.fragment");
+
+    std::vector<VertexBinding> vertexBindings = {
+        VertexBinding(0,sizeof(float)*3,std::vector<VertexAttribute>{VertexAttribute("POSITION",PixelFormat::R32G32B32_FLOAT,0)}),
+        VertexBinding(1,sizeof(float)*2,std::vector<VertexAttribute>{VertexAttribute("UV_COORDINATES",PixelFormat::R32G32_FLOAT,0)}),
     };
-    commandBuffer->bindVertexBuffers(0,vertexBuffers,offsets,strides,2);
-    commandBuffer->drawIndexed(3,1,0,0,0);
+    VertexDescription vertexDescription(vertexBindings);
+
+    DescriptorRange constantRange(DescriptorRangeType::UNIFORM_BUFFER,0,0,1,0);
+    DescriptorRange samplerRange(DescriptorRangeType::SAMPLER,1,0,1,0);
+    DescriptorRange instanceRanges[]
+    {
+        DescriptorRange{DescriptorRangeType::UNIFORM_BUFFER,2,0,1,0},
+        DescriptorRange{DescriptorRangeType::READONLY_RESOURCE,2,1,1,1}
+    };
+    PipelineInputMappingBuilder mappingBuilder(card);
+    mappingBuilder.addDescriptorTable(&constantRange,1).addDescriptorTable(&samplerRange,1).addDescriptorTable(instanceRanges,2);
+    auto bindings = mappingBuilder.build();
+    FramebufferDescription framebufferDesc;
+    framebufferDesc.colorFormats[0] = colorTarget->format();
+    framebufferDesc.depthFormat = depthTarget->format();
+    auto pipeline = std::unique_ptr<ShaderPipeline>(card->newShaderPipeline(vertexDescription,vertex.get(),fragment.get(),&bindings,PipelineState(),framebufferDesc));
+
+    auto commandBuffer = std::unique_ptr<CommandBuffer>(card->newCommandBuffer(QueueType::GRAPHICS));
+    auto finished = std::unique_ptr<Semaphore>(card->newSemaphore());
+
+    auto sampler = std::unique_ptr<Sampler>(card->newSampler());
+    auto texture = utilities::loadTexture("resources\\tests\\textures\\gradient.jpg",card);
+
+
+    auto resourceHeap = std::unique_ptr<DescriptorHeap>(card->newDescriptorHeap(DescriptorHeapType::RESOURCE,card->descriptorTableDetails().sampledTextureSize*500));
+    auto samplerHeap = std::unique_ptr<DescriptorHeap>(card->newDescriptorHeap(DescriptorHeapType::SAMPLER,card->descriptorTableDetails().samplerDescriptorSize*200));
+    uint32_t resourceHeapOffset = 0;
+    uint32_t samplerHeapOffset = 0;
+
+    auto table0Offset = resourceHeapOffset;
+    resourceHeap->writeUniformBufferDescriptor(resourceHeapOffset,globals.get(),0,globals->size());
+    resourceHeapOffset += card->descriptorTableDetails().sampledTextureSize;
+
+    auto table1Offset = samplerHeapOffset;
+    samplerHeap->writeSamplerDescriptor(samplerHeapOffset,sampler.get());
+    samplerHeapOffset += card->descriptorTableDetails().samplerDescriptorSize;
+
+    auto table2Offset = resourceHeapOffset;
+    resourceHeap->writeUniformBufferDescriptor(resourceHeapOffset,transform.get(),0,transform->size());
+    resourceHeapOffset += card->descriptorTableDetails().sampledTextureSize;
+    resourceHeap->writeSampledTextureDescriptor(resourceHeapOffset,texture.get());
+    resourceHeapOffset += card->descriptorTableDetails().sampledTextureSize;
+
+
+    commandBuffer->begin();
+
+    commandBuffer->bindDescriptorHeaps(resourceHeap.get(),samplerHeap.get());
+    commandBuffer->setViewPort(0,0,colorTarget->width(),colorTarget->height(),0.0f,1.0f);
+    commandBuffer->setScissors(Rectangle{0,0,colorTarget->width(),colorTarget->height()});
+    Attachment colorAttachment(colorTarget.get(),true,ClearValue{.5,.2,.1,1});
+    Attachment depthAttachment(depthTarget.get(),true,ClearValue{1,0});
+    commandBuffer->beginRendering(&colorAttachment,1,&depthAttachment,Rectangle{0,0,colorTarget->width(),colorTarget->height()});
+
+    Buffer* buffers[] = {triangleVerts.get(),triangleUVs.get()};
+    uint64_t offsets[] = {0,0};
+    uint64_t strides[] = {sizeof(glm::vec3),sizeof(glm::vec2)};
+    commandBuffer->bindVertexBuffers(0,buffers,offsets,strides,2);
+    commandBuffer->bindIndexBuffer(triangleIndices.get(),IndexBufferType::UINT_16,0);
+    commandBuffer->bindGraphicsPipeline(pipeline.get());
+
+    commandBuffer->setInputBindingTable(bindings.input(0).location(),table0Offset);
+    commandBuffer->setInputBindingTable(bindings.input(1).location(),table1Offset);
+    commandBuffer->setInputBindingTable(bindings.input(2).location(),table2Offset);
+    commandBuffer->drawIndexed(tindexes.size(),1,0,0,0);
 
     commandBuffer->endRendering();
-    TextureBarrier finishedRendering
-    {
-        .texture = colorTarget.get(),
-        .baseLayer = 0,
-        .layerCount = 1,
-        .baseMipLevel = 0,
-        .mipCount = 1,
-        .syncBefore = SyncStages::COLOR_TARGET_OUTPUT,
-        .syncAfter = SyncStages::COPY,
-        .flush = MemoryCaches::COLOR_TARGET,
-        .invalidate = MemoryCaches::COPY_READ,
-    };
-    commandBuffer->insertBarriers(&finishedRendering,1);
-    TextureBufferMapping mapping
-    {
-        .bufferOffset = 0,
-        .subresource =
-            {
-                .aspect = PixelAspect::COLOR,
-                .mipLevel = 0,
-                .baseArrayLayer = 0,
-                .layerCount = 1
-            },
-        .offset = {0,0,0},
-        .extent = {colorTarget->width(),colorTarget->height(),1}
-    };
-    commandBuffer->copyTextureToBuffer(colorTarget.get(),resultBuffer.get(),&mapping,1);
-    commandBuffer->end();
 
-    auto rawCB = commandBuffer.get();
-    SemaphoreValue signal{.semaphore = finished.get(),.value = 1};
-    SubmissionBatch submissionBatch
+    commandBuffer->end();
+    auto cmdBufferPtr = commandBuffer.get();
+    SemaphoreValue signal{.semaphore = finished.get(),.value =1};
+    SubmissionBatch batch
     {
-        .commandBuffers = &rawCB,
+        .waitSemaphores = nullptr,
+        .waitSemaphoreCount = 0,
+        .commandBuffers = &cmdBufferPtr,
         .commandBufferCount = 1,
         .signalSemaphores = &signal,
-        .signalSemaphoreCount = 1
+        .signalSemaphoreCount = 1,
     };
-    card->graphicsQueue()->submit(&submissionBatch,1);
+    card->graphicsQueue()->submit(&batch,1);
     finished->waitForValue(1);
-    lodepng::encode("C:\\Users\\jshelton\\Desktop\\results\\sandbox.png",resultBuffer->as<unsigned char>(),colorTarget->width(),colorTarget->height());
-    int i=0;
+    utilities::saveTexture("C:\\Users\\jshelton\\Desktop\\results\\output.png",colorTarget.get());
+    GTEST_FAIL();
+}
+
+TEST(ShaderPipeline, SandboxBindless)
+{
+    auto card = Slag::backend()->graphicsCard(0);
+
+    std::vector<glm::vec3> tverts = {{ -1.f, -1.f, 0.0f},{0.f,1.f, 0.0f},{1.f, -1.f, 0.0f}};
+    std::vector<glm::vec2> tuvs = {{0,1},{.5,0},{1,1}};
+    std::vector<glm::vec3> tnormals = {{0,0,1},{0,0,1},{0,0,1}};
+    std::vector<uint16_t> tindexes = {0,1,2};
+
+    auto triangleVerts = std::unique_ptr<Buffer>(card->newBuffer(tverts.size()*sizeof(glm::vec3),BufferCPUAccess::WRITE_ONLY));
+    auto triangleUVs = std::unique_ptr<Buffer>(card->newBuffer(tuvs.size()*sizeof(glm::vec2),BufferCPUAccess::WRITE_ONLY));
+    auto triangleNormals = std::unique_ptr<Buffer>(card->newBuffer(tnormals.size()*sizeof(glm::vec3),BufferCPUAccess::WRITE_ONLY));
+    auto triangleIndices = std::unique_ptr<Buffer>(card->newBuffer(tindexes.size()*sizeof(uint16_t),BufferCPUAccess::WRITE_ONLY));
+
+    auto tvertsPtr = triangleVerts->as<glm::vec3>();
+    memcpy(tvertsPtr,tverts.data(),tverts.size()*sizeof(glm::vec3));
+    auto tuvsPtr = triangleUVs->as<glm::vec2>();
+    memcpy(tuvsPtr,tuvs.data(),tuvs.size()*sizeof(glm::vec2));
+    auto tnormalsPtr = triangleNormals->as<glm::vec3>();
+    memcpy(tnormalsPtr,tnormals.data(),tnormals.size()*sizeof(glm::vec3));
+    auto tindicesPtr = triangleIndices->as<uint16_t>();
+    memcpy(tindicesPtr,tindexes.data(),tindexes.size()*sizeof(uint16_t));
+
+    auto vertex = slagTestsCreateShaderModule(card, "resources/tests/shaders/compiled/TexturedDepthBindless.vertex");
+    auto fragment = slagTestsCreateShaderModule(card, "resources/tests/shaders/compiled/TexturedDepthBindless.fragment");
+    std::vector<VertexBinding> vertexBindings =
+    {
+        VertexBinding(0,sizeof(float)*3,std::vector<VertexAttribute>{VertexAttribute("POSITION",PixelFormat::R32G32B32_FLOAT,0)}),
+        VertexBinding(1,sizeof(float)*2,std::vector<VertexAttribute>{VertexAttribute("UV_COORDINATES",PixelFormat::R32G32_FLOAT,0)})
+    };
     GTEST_FAIL();
 }
 
